9bc3594ba4a3b9e1ddaf3c7fd521bee0d962bdb2
Chris Tai <christopherctai@gmail.com>
2023-04-27 09:56:54 -0700

    Write function to get weather data

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-console.log('hello world! Trying webpack');
-
 async function getWeatherData(location) {
-    let response = await fetch();
+    try {
+        let response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=812a5522c00940c58f902241232604&q=${location}&days=3`);
+        let weatherData = await response.json();
+        console.log(weatherData);
+    } catch(error) {
+        console.log(error);
+    }
 }
+
+
+function processWeatherData(data) {
+    let weather;
+}
+
+
+getWeatherData('london');
+getForecastWeatherData('london');
