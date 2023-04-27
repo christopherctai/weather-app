51ac61f9c98a77215b036f7a9b95b17a63608c81
Chris Tai <christopherctai@gmail.com>
2023-04-27 10:19:43 -0700

    Return a processedWeatherData object from the processWeatherData function

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ async function getWeatherData(location) {
     try {
         let response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=812a5522c00940c58f902241232604&q=${location}&days=3`);
         let weatherData = await response.json();
-        console.log(weatherData);
         return weatherData;
     } catch(error) {
         console.log(error);
@@ -11,20 +10,20 @@ async function getWeatherData(location) {
 
 
 function processWeatherData(data) {
-    let conditionIcon = data.current.condition.icon;
-    let conditionText = data.current.condition.text;
-    let feelsLikeC = data.current.feelslike_c;
-    let feelsLikeF = data.current.feelslike_f;
-    let humidity = data.current.humidity;
-    let lastUpdated = data.current.last_updated;
-    let tempC = data.current.temp_c;
-    let tempF = data.current.temp_f;
-    let windKph = data.current.wind_kph;
-    let windMph = data.current.wind_mph; 
-    let forecast = data.forecast;
+    let processedWeatherData = {};
+    processedWeatherData.conditionIcon = data.current.condition.icon;
+    processedWeatherData.conditionText = data.current.condition.text;
+    processedWeatherData.feelsLikeC = data.current.feelslike_c;
+    processedWeatherData.feelsLikeF = data.current.feelslike_f;
+    processedWeatherData.humidity = data.current.humidity;
+    processedWeatherData.lastUpdated = data.current.last_updated;
+    processedWeatherData.tempC = data.current.temp_c;
+    processedWeatherData.tempF = data.current.temp_f;
+    processedWeatherData.windKph = data.current.wind_kph;
+    processedWeatherData.windMph = data.current.wind_mph; 
+    processedWeatherData.forecast = data.forecast;
 
-    console.log(conditionIcon, conditionText, feelsLikeC, feelsLikeF, humidity, lastUpdated,
-        tempC, tempF, windKph, windMph, forecast);
+    return processedWeatherData;
 }
 
 
