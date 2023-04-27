727d33a3f3a1136f83ba5f55020e51fabd886cc7
Chris Tai <christopherctai@gmail.com>
2023-04-27 10:09:17 -0700

    Write function to process weather data

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ async function getWeatherData(location) {
         let response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=812a5522c00940c58f902241232604&q=${location}&days=3`);
         let weatherData = await response.json();
         console.log(weatherData);
+        return weatherData;
     } catch(error) {
         console.log(error);
     }
@@ -10,9 +11,21 @@ async function getWeatherData(location) {
 
 
 function processWeatherData(data) {
-    let weather;
+    let conditionIcon = data.current.condition.icon;
+    let conditionText = data.current.condition.text;
+    let feelsLikeC = data.current.feelslike_c;
+    let feelsLikeF = data.current.feelslike_f;
+    let humidity = data.current.humidity;
+    let lastUpdated = data.current.last_updated;
+    let tempC = data.current.temp_c;
+    let tempF = data.current.temp_f;
+    let windKph = data.current.wind_kph;
+    let windMph = data.current.wind_mph; 
+    let forecast = data.forecast;
+
+    console.log(conditionIcon, conditionText, feelsLikeC, feelsLikeF, humidity, lastUpdated,
+        tempC, tempF, windKph, windMph, forecast);
 }
 
 
-getWeatherData('london');
-getForecastWeatherData('london');
+getWeatherData('london').then((response) => {processWeatherData(response)});
